d385f427491ee47b753c2caf34191a1a9e69fac1
melmae <43497997+melmae@users.noreply.github.com>
2025-03-30 14:05:48 -0500

    Handle TS

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 
 function App() {
-    const [user, setUser] = useState(localStorage.getItem('anicomplete-user') ? JSON.parse(localStorage.getItem('anicomplete-user')) :{username: '', id: 0, theme: 'default'})
+    const [user, setUser] = useState(localStorage.getItem('anicomplete-user') ? JSON.parse(localStorage.getItem('anicomplete-user') as string) : {username: '', id: 0, theme: 'default'})
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false);
 
@@ -56,7 +56,7 @@ function App() {
         fetch(url, options)
             .then(res => res.ok ? res.json() : Promise.reject(res))
             .then((data) => {
-                setData(data.data.MediaListCollection.lists.find(l => l.name === "Completed")?.entries || []);
+                setData(data.data.MediaListCollection.lists.find((l: { name: string }) => l.name === "Completed")?.entries || []);
                 setLoading(false);
             })
             .catch(console.log);
@@ -109,17 +109,6 @@ function App() {
         setData([]);
     }
 
-    function getColor() {
-        if (user.theme !== 'default')
-            return user.theme;
-
-        // console.log(window.matchMedia('(prefers-color-scheme: dark)'))
-        // if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
-        //     return '#fff';
-
-        return 'inherit';
-    }
-
     return (
         <div>
             <div className="flex gap-2 justify-center items-center w-full my-4">
@@ -137,16 +126,18 @@ function App() {
                 <Button className="clear" variant="outline" onClick={clearUsername} style={{color: color, borderColor: color}}>Clear</Button>
             </div>
             <p style={{color: color}}>Total read: {data.length}</p>
-            <p style={{color: color}} className="mb-2">Total mismatches: {data.filter(e => e.progress != e.media.chapters).length}</p>
+            <p style={{color: color}} className="mb-2">Total mismatches: {data.filter((e: {progress: string, media: {chapters: string}} )=> e.progress != e.media.chapters).length}</p>
             {loading && <p>Loading...</p>}
-            {data.filter(e => e.progress !== e.media.chapters)
-                .sort((e1, e2) => (e1.media.title.english || e1.media.title.romaji || '').localeCompare(e2.media.title.english || e2.media.title.romaji || ''))
-                .map((item) => {
+            {data.filter((e: {progress: string, media: {chapters: string}}) =>
+                    e.progress !== e.media.chapters)
+                .sort((e1: {media: {title: {english: string, romaji: string}}}, e2: {media: {title: {english: string, romaji: string}}}) =>
+                    (e1.media.title.english || e1.media.title.romaji || '').localeCompare(e2.media.title.english || e2.media.title.romaji || ''))
+                .map((item: {progress: string, media: {siteUrl: string, chapters: string, title: {english: string, romaji: string}, coverImage: {large: string}}}) => {
                     return (
                         <div className="w-full flex justify-center items-center m-2">
-                            <a className="w-2/3 min-w-xs" href={item.media.siteUrl} key={item.media.title} target="_blank" rel="noopener noreferrer">
+                            <a className="w-2/3 min-w-xs" href={item.media.siteUrl} key={item.media.title.english || item.media.title.romaji} target="_blank" rel="noopener noreferrer">
                                 <Card className="flex flex-row gap-3 justify-center items-center p-2">
-                                    <img src={item.media.coverImage.large} alt={item.media.title} style={{ height: '60px', width: '50px' }} />
+                                    <img src={item.media.coverImage.large} alt={item.media.title.english || item.media.title.romaji} style={{ height: '60px', width: '50px' }} />
                                     <p>{item.media.title.english || item.media.title.romaji}</p>
                                     <p>Progress: {`${item.progress}/${item.media.chapters || '?'}`}</p>
                                 </Card>
